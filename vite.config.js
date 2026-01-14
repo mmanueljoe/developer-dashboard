d020b4806120469b54e60f92aac8d28dffb17bd7
mmanueljoe <emmanuelletsu18@gmail.com>
2026-01-14 19:41:00 +0000

    chore(build automation): update vite config files to include and package.json to include custom scripts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+    base: '/',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: 'true',
+  }
 })
