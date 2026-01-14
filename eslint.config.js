70413f2e96ce2d060f3823c7d128251119169ba6
mmanueljoe <emmanuelletsu18@gmail.com>
2026-01-14 20:06:13 +0000

    chore(prettier): initialize ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite, 'prettier'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -24,7 +20,7 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'eqeqeq': 'warn',
+      eqeqeq: 'warn',
       'react/react-in-jsx-scope': 'off',
       'react/jsx-no-target-blank': 'off',
       'react/jsx-no-useless-fragment': 'off',
@@ -32,4 +28,4 @@ export default defineConfig([
       'react/jsx-no-duplicate-props': 'off',
     },
   },
-])
+]);
